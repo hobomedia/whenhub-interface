5f23f3f55ba4667a199c2732783df09dc3f7276b
Traci Fong <tre.fong1@gmail.com>
2018-01-17 07:56:49 -0800

    Add nav responsive nav bar

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,14 +1,40 @@
 import * as React from 'react';
-
 let styles = require('./Home.scss');
 
-export default class Home extends React.Component {
-  render() {
+
+// interface HomeProps {
+//   number: number
+// }
+
+export default class Home extends React.Component<any, {number2?: number, sidebarOpen: boolean}>{ 
+  constructor(props:any){
+    super(props)
+    this.state = {
+      sidebarOpen: true
+    }
+  }
+
+    render() {
     return (
-      <div>
-        <div className={styles.container} data-tid="container">
+      <div className={styles.box} data-tid="container">  
+        <div className="navbar navbar-inverse navbar-fixed-left">
+          <a className="navbar-brand" href="#"><img src={require("../../resources/Interface-Logo-btn.png")}/></a>
+          <ul className="nav navbar-nav">
+            <li><a href="#"><i className="fa fa-usd"></i><span>WhenWallet</span></a></li>
+            <li><a href="#"><i className="fa fa-bars"></i><span>Settings</span></a></li>
+            <li><a href="#"><i className="fa fa-history"></i><span>History</span></a></li>
+            <li><a href="#"><i className="fa fa-info"></i><span>Tour</span></a></li>
+            <li><a href="#"><i className="fa fa-envelope-o"></i><span>Support</span></a></li>
+            <li><a href="#"><i className="fa fa-question-circle"></i><span>FAQ</span></a></li>
+            <li><a href="#"><i className="fa fa-sign-out"></i><span>Log Out</span></a></li>
+
+          </ul>
         </div>
-      </div>
+        <div className="container">
+          <div className="row">
+          </div>
+        </div>       
+        </div>
     );
   }
 }
