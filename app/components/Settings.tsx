d33cf82c7a0d70009ab99e21179e07d7e6d7c1e9
Traci Fong <tre.fong1@gmail.com>
2018-03-07 02:10:03 -0800

    Add text to settings page

--- app/components/Settings.tsx
+++ app/components/Settings.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import Nav from './Nav';
 
 let styles = require('./Home.scss');
+let settingsStyles = require('./Settings.scss');
 
 export default class Settings extends React.Component<any, {}>{ 
   constructor(props:any){
@@ -15,7 +16,16 @@ export default class Settings extends React.Component<any, {}>{
           button={"menu"}
         />
         <div className={styles.container}>
-            Settings
+            <div className={settingsStyles.settings}>
+                <div className={settingsStyles.font}>Minimum Interface duration when you are the expert</div>
+                <div>slider</div>
+                <div className={settingsStyles.font}>Languages Fluently Spoken</div>
+                <div className={settingsStyles.lang}>English</div>
+                <div className={settingsStyles.lang}>Spanish</div>
+                <div className={settingsStyles.lang}>French</div>
+                <div className={settingsStyles.lang}>Spanish</div>
+
+            </div>
         </div>   
       </div>    
     );
