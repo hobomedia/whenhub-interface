e9aa758ba2bba0e57c0c121e7adbf40c17a12368
Traci Fong <tre.fong1@gmail.com>
2018-03-13 01:13:20 -0700

    add swiping image

--- app/components/Expert.tsx
+++ app/components/Expert.tsx
@@ -39,34 +39,36 @@ export default class BeExpert extends React.Component<any, {}>{
           />
           <div className={styles.container}>
             <div id={styles.bebackground}>
-              <div style={{backgroundColor: "rgba(0, 0, 0, 0.5)", marginTop: "260px", paddingTop: "13px", position: "absolute", width: '338px', height: "302px"}}>
-                <div style={{ color: "white", marginLeft: "10px", fontSize: "25px", fontWeight: 200}}>
-                  Name
+                <div className={expertStyles.swipe}>
+                    <img src={'../resources/swipe.png'} alt="swipe" />
                 </div>
-                  
+                <div style={{backgroundColor: "rgba(0, 0, 0, 0.5)", marginTop: "260px", paddingTop: "13px", position: "absolute", width: '338px', height: "302px"}}>
+                    <div style={{ color: "white", marginLeft: "10px", fontSize: "25px", fontWeight: 200}}>
+                    Name
+                    </div>
                     <div style={{ marginTop: "10px"}}>
                         <div style={{marginBottom: "10px"}}>
-                          <div className={expertStyles.fields}>
+                            <div className={expertStyles.fields}>
                             Expertise
-                          </div>
-                          <div className={expertStyles.fields}>
-                              Topic
-                          </div>
+                            </div>
+                            <div className={expertStyles.fields}>
+                                Topic
+                            </div>
                         </div>
                         <div>
-                          <div className={expertStyles.fields}>
+                            <div className={expertStyles.fields}>
                             W00.00($00.00) - Minimum 0 mins.
-                          </div>
+                            </div>
                         </div>
                         <div className={expertStyles.ratings}>
-                          <div style={{fontWeight: 100, marginLeft: "10px", color: "#FFF"}}>
+                            <div style={{fontWeight: 100, marginLeft: "10px", color: "#FFF"}}>
                             Self-rating for Topic
 
                             <div className="star-rating">
-                              {stars}
+                                {stars}
                             </div>
 
-                          </div>
+                            </div>
                         </div>
                         <div className={expertStyles.ratings} style={{position: "absolute", right: "10px", color: "#FFD700"}}>
                             <div style={{color: "#FFF", fontWeight: 100, marginBottom: "7px"}}>
@@ -80,8 +82,6 @@ export default class BeExpert extends React.Component<any, {}>{
                             Connect With Interace
                         </div>
                     </div>
-
-
               </div>
             </div>
           </div>
