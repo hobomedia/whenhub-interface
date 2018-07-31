cd417ad2661e45b5698e1a9cd5bad4abacdc5194
Traci Fong <tre.fong1@gmail.com>
2018-07-30 22:22:26 -0700

    fix indentation; remove nav border

--- app/components/Interface.tsx
+++ app/components/Interface.tsx
@@ -43,88 +43,83 @@ export class Interface extends React.Component<any, { interval: any, min: any, l
 
             //set local media to layout manager
             layoutManager.setLocalView(localMedia.getView());
-
             that.setState({ localMedia: localMedia, layoutManager: layoutManager })
         })
-            .then(function () {
-                //connect to websync
-                const client = new (window as any).fm.websync.client("https://v4.websync.fm/websync.ashx");
-                client.setDomainKey(new fm.icelink.Guid('b0e15424-ba55-489d-b62a-1d6e1aa5927d'));
-                client.connect({
-                    onSuccess: function (e: any) {
-                        console.log("connected to websync");
-                    },
-                    onFailure: function (e: any) {
-                        console.log("failed to connect to websync");
+        .then(function () {
+            //connect to websync
+            const client = new (window as any).fm.websync.client("https://v4.websync.fm/websync.ashx");
+            client.setDomainKey(new fm.icelink.Guid('b0e15424-ba55-489d-b62a-1d6e1aa5927d'));
+            client.connect({
+                onSuccess: function (e: any) {
+                    console.log("connected to websync");
+                },
+                onFailure: function (e: any) {
+                    console.log("failed to connect to websync");
+                }
+            });
+
+            //Join conference
+            let promise = new fm.icelink.Promise();
+            try {
+                let joinArgs = new fm.icelink.websync4.JoinConferenceArgs("/auto-signalling/" + `${connectionId}`);
+                joinArgs.setOnSuccess((args) => {
+                    console.log("success")
+                    promise.resolve({});
+                })
+                joinArgs.setOnFailure((args) => {
+                    console.log("fail")
+
+                    console.log(args.getException())
+                    promise.reject(args.getException());
+                })
+                joinArgs.setOnRemoteClient((remoteClient) => {
+
+                    //add remote media to layout manager
+                    let remoteMedia = new fm.icelink.RemoteMedia();
+                    let remoteView = remoteMedia.getView();
+                    if (remoteView != null) {
+                        remoteMedia.getViewSink().setViewScale(fm.icelink.LayoutScale.Contain);
+                        that.state.layoutManager.addRemoteView(remoteMedia.getId(), remoteView);
                     }
-                });
+                    //create connection to remote client
+                    const audioStream = new fm.icelink.AudioStream(that.state.localMedia, remoteMedia);
+                    const videoStream = new fm.icelink.VideoStream(that.state.localMedia, remoteMedia);
+                    const connection = new fm.icelink.Connection([audioStream, videoStream]);
 
+                    connection.setIceServers([
+                        new fm.icelink.IceServer("stun:turn.icelink.fm:3478"),
+                        new fm.icelink.IceServer("turn:turn.icelink.fm:443", "test", "pa55w0rd!")
+                    ]);
 
-                //Join conference
-                let promise = new fm.icelink.Promise();
-                try {
-                    let joinArgs = new fm.icelink.websync4.JoinConferenceArgs("/auto-signalling/" + `${connectionId}`);
-                    joinArgs.setOnSuccess((args) => {
-                        console.log("success")
-                        promise.resolve({});
-                    })
-                    joinArgs.setOnFailure((args) => {
-                        console.log("fail")
+                    fm.icelink.Log.setLogLevel(fm.icelink.LogLevel.Debug);
+                    fm.icelink.Log.registerProvider(new fm.icelink.ConsoleLogProvider(fm.icelink.LogLevel.Debug));
 
-                        console.log(args.getException())
-                        promise.reject(args.getException());
-                    })
-                    joinArgs.setOnRemoteClient((remoteClient) => {
-
-                        //add remote media to layout manager
-                        let remoteMedia = new fm.icelink.RemoteMedia();
-                        let remoteView = remoteMedia.getView();
-                        if (remoteView != null) {
-                            remoteMedia.getViewSink().setViewScale(fm.icelink.LayoutScale.Contain);
-                            that.state.layoutManager.addRemoteView(remoteMedia.getId(), remoteView);
+                    connection.addOnStateChange(function (c: fm.icelink.Connection) {
+                        var error = connection.getError();
+                        console.log(c.getState())
+                        if (c.getState() == fm.icelink.ConnectionState.Connected) {
+                            that.state.layoutManager.addRemoteView(remoteMedia.getId(), remoteMedia.getView());
+                            console.log("connected")
+                            that.setState({ remoteMedia: remoteMedia.getId() })
+                        } else if (c.getState() == fm.icelink.ConnectionState.Failing || c.getState() == fm.icelink.ConnectionState.Closing) {
+                            that.state.layoutManager.removeRemoteView(remoteMedia.getId());
+                            remoteMedia.destroy();
+                            console.log(error)
                         }
-                        //create connection to remote client
-                        const audioStream = new fm.icelink.AudioStream(that.state.localMedia, remoteMedia);
-                        const videoStream = new fm.icelink.VideoStream(that.state.localMedia, remoteMedia);
-                        const connection = new fm.icelink.Connection([audioStream, videoStream]);
-
-                        connection.setIceServers([
-                            new fm.icelink.IceServer("stun:turn.icelink.fm:3478"),
-                            new fm.icelink.IceServer("turn:turn.icelink.fm:443", "test", "pa55w0rd!")
-                        ]);
-
-                        fm.icelink.Log.setLogLevel(fm.icelink.LogLevel.Debug);
-                        fm.icelink.Log.registerProvider(new fm.icelink.ConsoleLogProvider(fm.icelink.LogLevel.Debug));
-
-                        connection.addOnStateChange(function (c: fm.icelink.Connection) {
-                            var error = connection.getError();
-                            console.log(c.getState())
-                            if (c.getState() == fm.icelink.ConnectionState.Connected) {
-                                that.state.layoutManager.addRemoteView(remoteMedia.getId(), remoteMedia.getView());
-                                console.log("connected")
-                                that.setState({ remoteMedia: remoteMedia.getId() })
-                            } else if (c.getState() == fm.icelink.ConnectionState.Failing || c.getState() == fm.icelink.ConnectionState.Closing) {
-                                that.state.layoutManager.removeRemoteView(remoteMedia.getId());
-                                remoteMedia.destroy();
-                                console.log(error)
-                            }
-                        });
-                        return connection
-                    })
-                    fm.icelink.websync4.ClientExtensions.joinConference(client, joinArgs);
-                }
-                catch (error) {
-                    console.log("new error", error)
-                    promise.reject(error);
-                }
-                that.setState({ client: client })
-                // return promise;
-            }).fail(function (error: any) {
-                console.log(error.message)
-            })
-
-    
-
+                    });
+                    return connection
+                })
+                fm.icelink.websync4.ClientExtensions.joinConference(client, joinArgs);
+            }
+            catch (error) {
+                console.log("new error", error)
+                promise.reject(error);
+            }
+            that.setState({ client: client })
+            // return promise;
+        }).fail(function (error: any) {
+            console.log(error.message)
+        })
     }
 
     componentWillUnmount(){
@@ -145,8 +140,6 @@ export class Interface extends React.Component<any, { interval: any, min: any, l
         this.props.history.push('/RateCall')
 
         this.state.layoutManager.removeRemoteView(this.state.remoteMedia)
-
-
     }
 
 
