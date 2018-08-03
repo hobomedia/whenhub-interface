549cb569c75f92ce7037c6cf3069a0e30e08661e
Traci Fong <tre.fong1@gmail.com>
2018-08-03 02:00:00 -0700

    create caller terms and conditions page

---
+++ app/components/TandC.tsx
@@ -0,0 +1,111 @@
+import * as React from 'react';
+import {connect} from 'react-redux';
+import { Link } from 'react-router-dom';
+import Checkbox from 'material-ui/Checkbox';
+
+const styles = require('./Home.scss');
+const tandcStyles = require('./TandC.scss');
+
+export class TandC extends React.Component<any, {termsCheck: boolean}>{ 
+  constructor(props:any){
+    super(props)
+
+    this.state = {
+        termsCheck: false
+    }
+  }
+
+  handleTermsChange(e: any, checked: any) {
+      if (this.state.termsCheck == false) {
+          this.setState({termsCheck: true})
+      }else {
+          this.setState({termsCheck: false})
+      }
+  }
+
+    render() {
+        return (
+        <div className={styles.container} style={{height: "600px"}}>
+            <div id={tandcStyles.background}>
+                <div style={{color: "white",textAlign: "center"}}>
+                    Interface Terms and Conditions
+                </div>
+
+                <table style={{margin: "auto"}}>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <table>
+                                    <tbody>
+                                        <tr>
+                                            <td>Maximum Amount: </td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+
+                <div style={{color: "white",textAlign: "center", fontWeight: 100}}>
+                    * Final amount charged may be less based on the duration of the call. The first minute of all calls is free. The minimum call duration is 15 minutes.
+                </div>
+
+                <div className={tandcStyles.termsHeader}>
+                    Call and Payment Terms
+                </div>
+                <div>
+                    <div className={tandcStyles.term}>
+                        <div className={tandcStyles.termHeader}>No Refund: </div><div className={tandcStyles.termbody}>Payments are automated and no refund is possible regardless of your satisfaction level with the call.</div>
+                    </div>
+
+                    <div className={tandcStyles.term}>
+                        <div className={tandcStyles.termHeader}>No Verification: </div><div className={tandcStyles.termbody}>None of the claims made by the person you are contacting have been verified by anybody.</div>
+                    </div>
+
+                    <div className={tandcStyles.term}>
+                        <div className={tandcStyles.termHeader}>No Guarantee: </div><div className={tandcStyles.termbody}>There is no guarantee that you will be satisfied with the outcome of the call.</div>
+                    </div>
+
+
+                </div>
+
+                <div className={tandcStyles.termsLink}>
+                    <Link to=""><span><i className="fa fa-external-link"></i> Review Terms and Conditions</span></Link>
+                </div>
+
+                <Checkbox
+                    color="default"
+                    checked={this.state.termsCheck}
+                    onChange={(e: any, checked: any) => this.handleTermsChange(e, checked)}
+                />
+
+
+                <div className={tandcStyles.consentText}>
+                    I understand the risk, agree with the terms, and wish to proceed with the call.
+                </div>
+
+                <div>
+                    Choose a payment option: 
+                </div>
+
+                <button style={{ backgroundColor: "#FFF", color: "black", width: "320px", marginTop: "10px", borderRadius: "20px", fontWeight: 100}} type="button" className="btn">
+                    When Pay
+                </button>
+
+
+            </div>
+        </div>       
+        );
+    }
+}
+
+const mapStateToProps = function (props: any, state: any) {
+    return {
+        profile: props.login.profile,
+        token: props.login.token,
+        bearer: props.login.bearer
+    }
+
+}
+export default connect(mapStateToProps)(TandC);
