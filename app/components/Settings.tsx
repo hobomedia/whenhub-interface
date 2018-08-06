9b2c0b591e4e85df23cdf34d1ecc8ec4c1088e59
Traci Fong <tre.fong1@gmail.com>
2018-08-06 01:22:16 -0700

    remove slider from settings

--- app/components/Settings.tsx
+++ app/components/Settings.tsx
@@ -3,7 +3,6 @@ import Nav from './Nav';
 import Switch from 'material-ui/Switch';
 import 'rc-slider/assets/index.css';
 
-const Slider = require('rc-slider/lib/Slider');
 const styles = require('./Home.scss');
 const settingsStyles = require('./Settings.scss');
 
@@ -13,10 +12,6 @@ export class Settings extends React.Component<any, {lang: Array<String>}>{
     this.state = {
       lang: []
     }
-  }
-
-  slider(value: any) {
-    console.log(value)
   }
 
   onSwitch(event: any, switched: Boolean) {
@@ -42,36 +37,7 @@ export class Settings extends React.Component<any, {lang: Array<String>}>{
           page={"Settings"}
         />
         <div className={styles.container}>
-
             <div className={settingsStyles.settings}>
-                <div className={settingsStyles.font}><i className="fa fa-hourglass"></i> Minimum Interface duration when you are the expert</div>
-                  <div className={settingsStyles.slider}>
-                  <Slider
-                    dots step={15} 
-                    min={15}
-                    max={75}
-                    marks={
-                      {
-                        15: "15",
-                        30: "30",
-                        45: "45",
-                        60: "60",
-                        75: "75"
-                      }
-                    }
-                    defaultValue={0} 
-                    onChange={this.slider}
-                    TrackStyle={{backgroundColor: '#37d3b4'}}
-                    dotStyle={{
-                      borderColor: '#37d3b4',
-                      backgroundColor: '#37d3b4'
-                    }}
-                    handleStyle={{
-                      borderColor: '#37d3b4',
-                      backgroundColor: '#37d3b4'
-                    }}
-                  />
-                  </div>
                 <div className={settingsStyles.font} style={{marginTop: '33px'}}><i className="fa fa-comment"></i> Languages Fluently Spoken</div>
                 <div className={settingsStyles.row}> 
                   <div className={settingsStyles.lang}>English</div>
@@ -115,7 +81,11 @@ export class Settings extends React.Component<any, {lang: Array<String>}>{
                 </div>
 
                 <div className={settingsStyles.font} style={{marginTop: '20px'}}><i className="fa fa-comment"></i> Subscriptions</div>
+                  <div>No subscriptions</div>
 
+                <button style={{ backgroundColor: "#37d3b4", color: "white", width: "320px", marginTop: "10px", borderRadius: "20px", fontWeight: 100 }} type="button" className="btn">
+                  Update Profile
+                </button>
 
             </div>
         </div>   
