3a0a7a9ba0f9c290fd77340bf390083ec995c838
Traci Fong <tre.fong1@gmail.com>
2018-08-15 01:11:06 -0700

    Create accept call notification

---
+++ app/components/Accept.tsx
@@ -0,0 +1,78 @@
+import * as React from 'react';
+import { connect } from 'react-redux';
+
+const acceptStyles = require('./Accept.scss');
+
+export class Accept extends React.Component<any, {}>{
+    constructor(props: any) {
+        super(props)
+
+    }
+
+    onDecline() {
+
+    }
+
+    onAccept(){
+
+    }
+
+
+    render() {
+        return (
+            <div className={acceptStyles.lower}>
+                <div>
+                    <img className={acceptStyles.picture} src={"../resources/expert_2.jpg"} alt="Avatar" />
+                    <div style={{display: "inline-block", verticalAlign: "top", width: "212px", height: "126px", paddingTop: "16px"}}>
+                        <div>
+                            Caller Name
+                        </div>
+                        <div>
+                            Is inviting you to Interface for ___ mins. (&#65510;) 0.00
+                        </div>
+                    </div>
+
+                </div>
+                <div className={acceptStyles.reason}>
+                    Reason for call
+                </div>
+                <div className={acceptStyles.cancelWell}>
+                    <div style={{display: "inline-block", paddingLeft: "10px"}}>
+                        <div>
+                            Fees
+                        </div>
+                        <div>
+                            Network: 0.00%
+                        </div>
+                    </div>
+                    <div style={{display: "inline-block", paddingLeft: "48px"}}>
+                        <button className={acceptStyles.accept + ` btn`} style={{borderRadius: "20px", display: "inline-block"}} type="button" onClick={this.onAccept.bind(this)}>
+                            Accept
+                        </button>
+
+                        <button className={acceptStyles.cancel + ` btn`} style={{borderRadius: "20px", display: "inline-block"}} type="button" onClick={this.onDecline.bind(this)}>
+                            Decline
+                        </button>
+                    </div>
+
+
+                </div>
+
+            </div>
+        );
+    }
+}
+
+const mapStateToProps = function (props: any, state: any) {
+    return {
+        profile: props.login.profile,
+        token: props.login.token,
+        bearer: props.login.bearer,
+        wallet: props.getWalletAmount.walletAmount,
+        interface: props.startInterface.start
+    }
+
+}
+
+
+export default connect(mapStateToProps)(Accept);
